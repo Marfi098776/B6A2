bea94cfd4d85cb4e88e71c79ee2a9cc5007f0136
Marfi098776 <mabrurgotech@gmail.com>
2026-01-16 09:23:52 +0600

    added 2nd commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from "express"
-import { Pool } from "pg"
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
-const app = express()
-const port = 5000
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const app = express();
+const port = 5000;
 app.use(express.json());
 
 const pool = new Pool({
-    connectionString: `postgresql://neondb_owner:npg_NMLRIHX8EPp4@ep-square-mode-ahd6rv3c-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+    connectionString: `${process.env.CONNECTION_STR}`
 })
 
 const initDB = async() => {
@@ -20,6 +24,17 @@ const initDB = async() => {
         role VARCHAR(50) NOT NULL
         )
         `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS vehicles(
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(100) NOT NULL,
+            type VARCHAR(200),
+            registration_number INT UNIQUE NOT NULL,
+            daily_rent_price INT NOT NULL,
+            availability_status VARCHAR(100) NOT NULL
+            )
+            `);
+        
 }
 
 initDB()
